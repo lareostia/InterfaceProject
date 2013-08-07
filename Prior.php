ef2e4baa7bdd5533e10929b9b7f668896f4054d8
Sergio Peleato <speleato@gmail.com>
2013-08-07 12:15:01 -0700

    Latest
    Prior.php not working properly, but it will do

---
+++ Prior.php
@@ -0,0 +1,15 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>mainPage</title>
+	<meta name="author" content="pele" />
+	<link href="Styles/Options.css" rel="stylesheet" type="text/css" />
+	<!-- Date: 2013-08-04 -->
+</head>
+<body>
+<body>
+<?php
+// We will recover the information about the user in this section, and display the given information
+echo "<p>Hello world</p>";
+echo "<p>string</p>";
+?>
+</body>
